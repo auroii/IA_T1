f914f21f8c2c0f022f6ef44a35522b1f6918a832
auroii <auromartsojr@gmail.com>
2018-05-08 10:32:12 -0300

    Add files via upload

---
+++ bfs.cpp
@@ -0,0 +1,140 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+const int dc[4] = {0,0,1,-1}; //dir colums
+const int dr[4] = {1,-1,0,0}; // dir rows
+const int pixels = 482;
+const int expect_row = 479;
+const int expect_col = 239;
+const int start_row_global = 3;
+const int start_col_global = 239;
+
+
+
+bool BFS(int start_row, int start_col, int **graph, int **vis) {
+	
+	pair<int, int> start = make_pair(start_row, start_col);
+	
+	queue<pair<int, int> > q;
+	
+	q.push(start);
+
+	while(!q.empty()) {
+		pair<int, int> node = q.front();
+		
+		if(node.first == expect_row and node.second == expect_col) {
+			return true;
+		}
+		q.pop();
+		
+		for(int i = 0; i < 4; ++i) {
+			int n_row = node.first + dr[i];
+			int n_col = node.second + dc[i];
+			
+			if(n_row < 0 or n_row >= pixels) continue;
+			if(n_col < 0 or n_col >= pixels) continue;
+
+			pair<int, int> current = make_pair(n_row, n_col);
+			
+			if(!vis[current.first][current.second] and !graph[current.first][current.second]) {
+				q.push(current);
+				vis[current.first][current.second] = vis[node.first][node.second] + 1;
+			}
+		}
+	}
+	return false;
+}
+	
+	
+vector<pair<int, int> > reverse_way(int **vis) {
+	
+	vector<pair<int, int> > res;	
+	
+	int value = vis[expect_row][expect_col];
+
+	int ni = expect_row, nj = expect_col;
+
+	while(ni != start_row_global || nj != start_col_global) {
+		if(value < 5) break;
+		//fprintf(stderr, "%d\n", value);
+		for(int i = 0; i < 4; ++i) {
+			int x = ni + dr[i];
+			int y = nj + dc[i];
+
+			if(vis[x][y] == value-1) {
+				res.push_back(make_pair(x, y));
+				value--;
+				ni = x;
+				nj = y;
+				break;
+			}
+		}
+	}
+
+	return res;	
+}
+			
+
+	
+	
+enum {
+	PROGNAME, FILENAME
+};
+
+
+int main(int argc, char *argv[]) {
+	FILE *fp = fopen(argv[FILENAME], "r");
+	
+	
+
+	int **graph = new int*[pixels];
+	for(int i = 0; i < pixels; ++i) {
+		graph[i] = new int[pixels];
+	}
+
+	int **vis = new int*[pixels];
+	
+	for(int i = 0; i < pixels; ++i) {
+		vis[i] = new int[pixels];
+	}
+
+	for(int i = 0; i < pixels; ++i) memset(vis[i], 0, sizeof(int) * pixels);
+	
+
+
+	for(int i = 0; i < pixels; ++i) {
+		for(int j = 0; j < pixels; ++j) {
+			fscanf(fp, "%d", &graph[i][j]);
+		}
+	}
+
+	vector<pair<int, int> > path;
+	
+	if(!BFS(start_row_global, start_col_global, graph, vis)) fprintf(stderr, "ERROR\n");
+	else path = reverse_way(vis);
+	
+	
+	printf("%lu\n", path.size());
+	
+	for(int i = path.size()-1; i >= 0; --i)
+		printf("%d %d\n", path[i].first, path[i].second);
+	
+	for(int i = 0; i < pixels; ++i) {	
+		delete [] graph[i];
+	}
+	
+	delete [] graph;
+	
+	for(int i = 0; i < pixels; ++i) {	
+		delete [] vis[i];
+	}
+	
+	delete [] vis;
+
+
+	fclose(fp);
+
+	return 0;
+} 
